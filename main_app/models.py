e360799b6ab85debf23721f1ee03c8c0a051c9b1
Cristian Nambo <nambocristian10@gmail.com>
2022-07-12 00:32:08 -0500

    wip: adds car model and minor styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    hp = models.IntegerField()
+    torque = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.name
